e1458738df3e8a44fd368b93421021bcd1f8920c
JaskiewiczK <116189743+JaskiewiczK@users.noreply.github.com>
2024-05-07 20:44:48 +0200

    Delete src/main/java/szp/repository/AssignmentStateRepository.java

--- src/main/java/szp/repository/AssignmentStateRepository.java
+++
@@ -1,9 +0,0 @@
-package szp.repository;
-
-import szp.model.AssignmentStateModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AssignmentStateRepository extends JpaRepository<AssignmentStateModel, Integer> {
-}
