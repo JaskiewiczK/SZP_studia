b5a9bc5f78de0052af6570167fcdaa89893d0cf0
Krzysztof Czerwiński <krzys.czerwinski@gmail.com>
2024-05-20 23:07:41 +0200

    Create vacations view and implement filtering mechanism

--- src/main/java/szp/repository/VacationRepository.java
+++ src/main/java/szp/repository/VacationRepository.java
@@ -1,6 +1,8 @@
 package szp.repository;
 
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import szp.model.VacationModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +13,8 @@ import java.util.List;
 @Repository
 public interface VacationRepository extends JpaRepository<VacationModel, Integer> {
     List<VacationModel> findByEmployee_EmployeeId(Integer employeeId);
+    List<VacationModel> findAllByBeginningIsGreaterThanEqualAndEndingLessThanEqual(Date beginning, Date ending);
+
+    @Query("from VacationModel vm where (vm.beginning between :beginning and :ending) or (vm.ending between :beginning and :ending)")
+    List<VacationModel> findAllWhereVacationDateIncludesDates(@Param(value = "beginning") Date beginning, @Param(value = "ending") Date ending);
 }
